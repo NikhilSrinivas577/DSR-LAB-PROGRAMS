001ec64e3f52fef5d92009011046754dff3d7682
Nikhil Srinivas.M <52965612+NikhilSrinivas577@users.noreply.github.com>
2019-11-11 11:57:28 +0530

    Add files via upload

---
+++ 1a.R
@@ -0,0 +1,8 @@
+path="/home/bmsce/Downloads/DSR_LAB_PROGRAMS-master/DSRLAB/DATA_SET"
+setwd(path)
+dataval=read.csv("iris.csv")
+dataval
+#sepal.length
+#sepal.width
+
+plot(dataval$sepal.length,dataval$sepal.width)
