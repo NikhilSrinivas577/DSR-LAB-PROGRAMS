fbb5792ed97244563f3473772ed6ccd783014fea
Nikhil Srinivas.M <52965612+NikhilSrinivas577@users.noreply.github.com>
2019-11-11 12:13:25 +0530

    Nikhil Srinivas

--- lab prog 1a.R
+++ lab prog 1a.R
@@ -2,8 +2,5 @@ path="/home/bmsce/Downloads/DSR_LAB_PROGRAMS-master/DSRLAB/DATA_SET"
 setwd(path)
 dataval=read.csv("iris.csv")
 dataval
-#sepal.length
-#sepal.width
-
 plot(dataval$sepal.length,dataval$sepal.width)
 print("This is done by Nikhil Srinivas M")
