da3c35a616a96f41e8e48d84c2aac4b3f3162e31
Nikhil Srinivas.M <52965612+NikhilSrinivas577@users.noreply.github.com>
2019-11-11 12:14:35 +0530

    Nikhil Srinivas M

--- 1b.R
+++ 1b.R
@@ -5,3 +5,4 @@ bankdata
 val_new=vector(mode="numeric",length =length(bankdata$Wind))
 bankdata$num<- seq.int(nrow(bankdata))
 write.table(bankdata,file="file.csv", sep= "\t", row.names=FALSE)
+print("This is done by Nikhil Srinivas M")
